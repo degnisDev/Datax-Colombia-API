720f6ba926c2ef33a84015f5e7acc23869a351c5
degnisDev <degnisdev@gmail.com>
2026-03-30 14:46:37 -0500

    Terminamos de construir el panel Gral

--- fronted/dashboard.py
+++ fronted/dashboard.py
@@ -119,6 +119,73 @@ st.plotly_chart(fig_pib, use_container_with=True) #  use_container_with=True = o
 # --- FILA CON 2 GRAficas LADO A LADO ---
 col_a, col_b = st.columns(2)
 
+with col_a:
+    # GRAFICA 2: Inflacion - usamos la linea porque es una serie temporal continua
+    fig_inf = px.line(
+        df,
+        x='anio',
+        y="Inflacion_Anual_pct",
+        color="Presidente",
+        color_discrete_map=COLOR_MAP,
+        title="Inflacion Anual (%)",
+        markers=True, # Muestra un punto en cada a;o
+        labels={"anio": "Año", "Inflacion_Anual_pct": "Inflacion (%)"}
+    )
+    fig_inf.update_layout(plot_bgcolor="rgba(0,0,0,0)", paper_bgcolor="rgba(0,0,0,0)")
+    st.plotly_chart(fig_inf, use_container_with=True)
+
+
+with col_b:
+    # GRAFICA 3: Desempleo - tambien serie temporal, usamos linea con area rellena
+    fig_desemp = px.area(
+        df,
+        x="anio",
+        y="Desempleo_pct_Total",
+        color="Presidente",
+        color_discrete_map=COLOR_MAP,
+        title="👷 Tasa de Desempleo (%)",
+        labels={"anio": "Año", "Desempleo_pct_Total": "Desempleo (%)"}
+    )    
+
+    fig_desemp.update_layout(plot_bgcolor="rgba(0,0,0,0)", paper_bgcolor="rgba(0,0,0,0)")
+    st.plotly_chart(fig_desemp, use_container_with=True)
+
+st.divider()
+
+# ---- FILA CON 2 GRAFICAS ---
+col_c, col_d = st.columns(2)
+
+with col_c:
+    # GRAFICA 4: Gastp en Educacion - barras para comparar magnitudes entre presidentes
+    fig_edu = px.bar(
+        df,
+        x="anio",
+        y="Gasto_Educacion_pct_Presupuesto",
+        color="Presidente",
+        color_discrete_map=COLOR_MAP,
+        title="Gasto en Educacion (% del Presupuesto Gral)",
+        labels={"anio": "Año", "Gasto_Educacion_pct_Presupuesto": "Educación (%)"}
+    ) 
+
+    fig_desemp.update_layout(plot_bgcolor="rgba(0,0,0,0)", paper_bgcolor="rgba(0,0,0,0)")
+    st.plotly_chart(fig_desemp, use_container_with=True)
+
+with col_d:
+    # GRAFICA 5: Muertes en conflicto - barras, facil de ver los picos historicos
+    fig_conf = px.bar (
+        df,
+        x="anio",
+        y="Muertes_Conflicto",
+        color="Presidente",
+        color_discrete_map=COLOR_MAP,
+        title="Muertes en Conflicto Armado",
+        labels={"anio": "Año", "Muertes_Conflicto": "Muertes"}
+    ) 
+
+    fig.conf.update_layout(plot_bgcolor="rgba(0,0,0,0)", paper_bgcolor="rgba(0,0,0,0)")
+    st.plotly_chart(fig_conf, use_container_width=True)
+
+
 
 
 
