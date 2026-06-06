b01e14953fc5a439a02c7c4812c5f586ee6fbe44
degnisDev <degnisdev@gmail.com>
2026-06-06 16:52:47 -0500

    Conectando Frontend con API de Render

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 /**
  * Configuración global del Frontend
  */
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://datax-colombia-api.onrender.com";
 
 export default API_BASE_URL;
